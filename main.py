38c1a89cbf84aa1d0577556a72cce1c3b326e5bc
Latifah <tifa-p@live.co.uk>
2019-11-03 16:04:17 +0000

    implemented cost function

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import matplotlib.pyplot as plt
 
 
-theta0 = 1
-theta1 = 0.5
-x = [1,2,3,4,5]
+theta0 = 2
+theta1 = 0.203
+x = [2104,1416,1534,852]
+y = [460,232,315,178]
 arr = []
+learning = 0.02
+
+def cost(thetaA,thetaB,input,output):
+	for i,j in zip(input, output):
+		sumSq =((thetaA +(thetaB*i)) - j)**2
+	return 1/2*len(input)*sumSq
+
+def gradientDescent():
+	return
 
 if __name__ == "__main__":
-	# print(ans)
-	for n in x:
-		arr.append(theta0 + (theta1 * n))
+	print(cost(theta0,theta1,x,y))
+	for i in x:
+		arr.append(theta0 + (theta1 * i))
 
+
+	plt.plot(x,y, marker='x', linestyle='None')
 	plt.plot(x,arr)
 	plt.show()
