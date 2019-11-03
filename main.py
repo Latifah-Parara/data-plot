b91742efd185eec005043470e44d33dcf1608239
Latifah <tifa-p@live.co.uk>
2019-11-03 01:25:36 +0000

    hypothesis calculation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-theta0 = 0
+theta0 = 1
 theta1 = 0.5
 x = [1,2,3,4,5]
-ans = [theta0+(theta1*x)]
-# def hypothesis(x):
-# 	ans = theta0+(theta1*x)
-# 	print(ans)
-
+arr = []
 
 if __name__ == "__main__":
 	# print(ans)
-	plt.plot(ans)
+	for n in x:
+		arr.append(theta0 + (theta1 * n))
+
+	plt.plot(x,arr)
 	plt.show()
